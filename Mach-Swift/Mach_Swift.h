a3a08a4a74f53d3f984a4423fdf41af39f2c4510
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-04-09 19:50:57 +0900

    Add empty project

---
+++ Mach-Swift/Mach_Swift.h
@@ -0,0 +1,19 @@
+//
+//  Mach_Swift.h
+//  Mach-Swift
+//
+//  Created by Daisuke T on 2019/04/09.
+//  Copyright © 2019 Mach-Swift. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Mach_Swift.
+FOUNDATION_EXPORT double Mach_SwiftVersionNumber;
+
+//! Project version string for Mach_Swift.
+FOUNDATION_EXPORT const unsigned char Mach_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mach_Swift/PublicHeader.h>
+
+
---
+++ Mach-SwiftTests/Mach_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  Mach_SwiftTests.swift
+//  Mach-SwiftTests
+//
+//  Created by Daisuke T on 2019/04/09.
+//  Copyright © 2019 Mach-Swift. All rights reserved.
+//
+
+import XCTest
+@testable import Mach_Swift
+
+class Mach_SwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
